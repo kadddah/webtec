5e0519e4dc2ba6df781abb9d2051e19d9dcde282
Katharina Imhof <kathaimhof@yahoo.de>
2013-04-17 20:07:16 +0200

    extract header/footer, include php

---
+++ webtechnologien/WebContent/footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Logbook: Bootstrap, from Twitter</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="logbook webtec">
+<meta name="author" content="Sven Weiser, Agnes Klein, Katharina Imhof">
+
+<!-- Le styles -->
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="bootstrap/css/footer.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144"
+	href="ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114"
+	href="ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72"
+	href="ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed"
+	href="ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="favicon.ico">
+</head>
+
+<body>
+
+	 <div id="push"></div>
+    </div>
+
+    <div id="footer">
+      <div class="container">
+        <p class="muted counter">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+      </div>
+    </div>
+	
+</body>
+</html>
--- webtechnologien/WebContent/logbook.php
+++ webtechnologien/WebContent/logbook.php
@@ -1,4 +1,5 @@
 <!-- WEBTEC Aufgabe Bootstrap -->
+<!-- WEBTEC Aufgabe PHP -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,12 +11,7 @@
 
 <!-- Le styles -->
 <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-<style>
-body {
-	padding-top: 60px;
-	/* 60px to make the container go all the way to the bottom of the topbar */
-}
-</style>
+
 <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -37,27 +33,8 @@ body {
 
 <body>
 
-	<div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container">
-				<button type="button" class="btn btn-navbar" data-toggle="collapse"
-					data-target=".nav-collapse">
-					<span class="icon-bar"></span> <span class="icon-bar"></span> <span
-						class="icon-bar"></span>
-				</button>
-				<a class="brand" href="#">Project name</a>
-				<div class="nav-collapse collapse">
-					<ul class="nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="#contact">Contact</a></li>
-					</ul>
-				</div>
-				<!--/.nav-collapse -->
-			</div>
-		</div>
-	</div>
-
+	<?php include("header.php"); ?>
+	
 	<div class="container">
 
 		<form class="navbar-form pull-left">
@@ -148,6 +125,8 @@ body {
 
 	</div>
 	<!-- /container -->
+	
+	<?php include("footer.php"); ?>
 
 	<!-- Le javascript
     ================================================== -->
